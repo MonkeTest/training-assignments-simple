06ad9ae13a790fef2c03aacbfb180f701322752e
Lennert Guarraci <lennert.guarraci@hva.nl>
2021-06-20 12:57:50 +0200

    Double code in savings and checkingsaccount removed

--- src/java/eu/sig/training/ch04/CheckingAccount.java
+++ src/java/eu/sig/training/ch04/CheckingAccount.java
@@ -13,13 +13,7 @@ public class CheckingAccount {
             throw new BusinessException("Limit exceeded!");
         }
         // 2. Assuming result is 9-digit bank account number, validate 11-test:
-        int sum = 0;
-        for (int i = 0; i < counterAccount.length(); i++) {
-            char character = counterAccount.charAt(i);
-            int characterValue = Character.getNumericValue(character);
-            sum = sum + (9 - i) * characterValue;
-        }
-        if (sum % 11 == 0) {
+        if (Accounts.isValid(counterAccount)) {
             // 3. Look up counter account and make transfer object:
             CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
             Transfer result = new Transfer(this, acct, amount);
@@ -28,7 +22,6 @@ public class CheckingAccount {
             throw new BusinessException("Invalid account number!");
         }
     }
-
     public void addInterest() {
         Money interest = balance.multiply(INTEREST_PERCENTAGE);
         if (interest.greaterThan(0)) {
--- src/java/eu/sig/training/ch04/SavingsAccount.java
+++ src/java/eu/sig/training/ch04/SavingsAccount.java
@@ -6,22 +6,12 @@ public class SavingsAccount {
     private Money balance = new Money();
     private CheckingAccount registeredCounterAccount;
 
-    public Transfer makeTransfer(String counterAccount, Money amount) 
-        throws BusinessException {
-        // 1. Assuming result is 9-digit bank account number, validate 11-test:
-        int sum = 0; // <1>
-        for (int i = 0; i < counterAccount.length(); i++) {
-            char character = counterAccount.charAt(i);
-            int characterValue = Character.getNumericValue(character);
-            sum = sum + (9 - i) * characterValue;
-        }
-        if (sum % 11 == 0) {
-            // 2. Look up counter account and make transfer object:
+    public Transfer makeTransfer(String counterAccount, Money amount)
+            throws BusinessException {
+        if (Accounts.isValid(counterAccount)) {
             CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
             Transfer result = new Transfer(this, acct, amount); // <2>
-            // 3. Check whether withdrawal is to registered counter account:
-            if (result.getCounterAccount().equals(this.registeredCounterAccount)) 
-            {
+            if (result.getCounterAccount().equals(this.registeredCounterAccount)) {
                 return result;
             } else {
                 throw new BusinessException("Counter-account not registered!");
