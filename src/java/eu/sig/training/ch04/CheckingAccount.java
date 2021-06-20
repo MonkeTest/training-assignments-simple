2e1b10215d04c7f0e80cf9039ecc9ede247c41af
Lennert Guarraci <lennert.guarraci@hva.nl>
2021-06-20 15:41:27 +0200

    savings refactored

--- src/java/eu/sig/training/ch04/CheckingAccount.java
+++ src/java/eu/sig/training/ch04/CheckingAccount.java
@@ -2,7 +2,6 @@ package eu.sig.training.ch04;
 
 // tag::CheckingAccount[]
 public class CheckingAccount extends Account{
-    private static final float INTEREST_PERCENTAGE = 0.01f;
     private int transferLimit = 100;
 
     @Override
--- src/java/eu/sig/training/ch04/SavingsAccount.java
+++ src/java/eu/sig/training/ch04/SavingsAccount.java
@@ -2,12 +2,11 @@ package eu.sig.training.ch04;
 
 // tag::SavingsAccount[]
 public class SavingsAccount extends Account {
-    private static final float INTEREST_PERCENTAGE = 0.04f;
     private CheckingAccount registeredCounterAccount;
 
     public Transfer makeTransfer(String counterAccount, Money amount) throws BusinessException {
         Transfer result = super.makeTransfer(counterAccount, amount);
-        if (result.getCounterAccount().equals(this.registeredCounterAccount)) {
+        if (result.getCounterAccount().equals(registeredCounterAccount)) {
             return result;
         } else {
             throw new BusinessException("Not Registered!");
